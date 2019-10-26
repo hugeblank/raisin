2d63831137790b320a1621a859aeb73f552cabeb
hugeblank <toddlangejr@gmail.com>
2019-10-26 13:52:40 -0700

    Update raisin.lua
    Once and for all resolve the whole dead coroutines issue.

--- raisin.lua
+++ raisin.lua
@@ -1,5 +1,5 @@
 --[[ Raisin by Hugeblank
-    This code is my own, but I will let you use it so long as you don't redistribute this manager for
+    This code is my property, but I will let you use it so long as you don't redistribute this manager for
     monetary gain and leave this comment block untouched. Add/remove code as you wish. Should you decide to freely
     distribute with additional modifications, please credit yourself. :)
     
@@ -23,11 +23,9 @@ local assert = function(condition, message, level) -- Local assert function that
 end
 
 local isgroup = function(group)
-    if group then
-        for i = 1, #groups do
-            if group == groups[i].instance then
-                return groups[i]
-            end
+    for i = 1, #groups do
+        if group == groups[i].instance then
+            return groups[i]
         end
     end
     return false
@@ -47,26 +45,6 @@ local interface = function(internal)
         setPriority = function(value)
             assert(type(value) == "number", "Invalid argument #1 (number expected, got "..type(value)..")")
             internal.priority = value
-        end,
-        remove = function()
-                -- Scan groups
-                for i = 1, #groups do
-                    if groups[i] == internal then
-                        table.remove(groups, i)
-                        return true
-                    end
-                end
-                -- Scan threads
-                for i = 1, #groups do
-                    local threads = groups[i].threads
-                    for j = 1, #threads do
-                        if threads[j] == internal then
-                            table.remove(groups[i].threads, j)
-                            return true
-                        end
-                    end
-                end
-            return false
         end
     }
 end
@@ -81,11 +59,10 @@ end
 
 this.thread = function(func, priority, group) -- Function for thread adding
     priority = priority or 0
+    group = isgroup(group or groups[1].instance)
     assert(type(func) == "function", "Invalid argument #1 (function expected, got "..type(func)..")")
     assert(type(priority) == "number", "Invalid argument #2 (number expected, got "..type(priority)..")")
-    local temp = isgroup(group)
-    assert(temp or group == nil, "Invalid argument #3 (valid group expected, got "..type(group)..")")
-    group = temp or groups[1]
+    assert(group, "Invalid argument #3 (valid group expected)")
     func = coroutine.create(func) -- Create a coroutine out of the function
     local internal = {coro = func, queue = {}, priority = priority, enabled = true, event = nil}
     internal.instance = interface(internal)
@@ -93,8 +70,7 @@ this.thread = function(func, priority, group) -- Function for thread adding
     return internal.instance
 end
 
-local runInternal = function(listener, groups, dead) -- Function to execute thread management
-    assert(type(listener) == "function", "Invalid argument #1 (function expected, got "..type(listener)..")", 1)
+local runInternal = function(groups, dead) -- Function to execute thread management
     assert(dead == nil or type(dead) == "number", "Invalid argument #1 (number expected, got "..type(dead)..")", 1)
     if not dead or dead < 0 then
         dead = 0
@@ -114,16 +90,19 @@ local runInternal = function(listener, groups, dead) -- Function to execute thre
         end
         return sorted
     end
-    local function resume(coro, event) -- General function to resume a coroutine with the following event
+    local function resume(coro, event)
         local suc, err = coroutine.resume(coro, table.unpack(event))
-        assert(suc, err, 1) -- Crash and burn if the coroutine crashed
-        return err -- Return the event filter
+        assert(suc, err, 1)
+        if suc then
+            return err    
+        end
     end
     local e = {} -- Event variable
-    local static, static_dead = {}, 0
+    local origin = {}
+    local totalDead = 0
     for i = 1, #groups do -- For each group
-        for j = 1, #groups[i].threads do -- Save as a static thread
-            static[#static+1] = groups[i].threads[j]
+        for j = 1, #groups[i].threads do
+            origin[#origin+1] = groups[i].threads[j] -- Create a table of threads that originated before execution
         end
     end
     while true do -- Begin thread management
@@ -133,9 +112,9 @@ local runInternal = function(listener, groups, dead) -- Function to execute thre
             if s_groups[i].enabled then -- If the group is enabled
                 for j = 1, #s_threads do -- For each sorted thread
                     local thread = s_threads[j]
-                    if thread.enabled and coroutine.status(thread.coro) == "suspended" and (thread.event == nil or thread.event == e[1]) then 
+                    if thread.enabled and coroutine.status(thread.coro) == "suspended" and (thread.event == nil or thread.event == e[1] or e[1] == "terminate") then 
                     -- There's a lot going on here, a newline was a must.
-                    -- If the thread is enabled, and the thread is suspended and the target event is either nil, or equal to the event detected THEN
+                    -- If the group is enabled and the thread is enabled, and the thread is suspended and the target event is either nil, or equal to the event detected, or equal to terminate
                         while #thread.queue ~= 0 do -- until the queue is empty
                             if thread.event == nil or thread.event == thread.queue[1][1] then -- If the target event is nil or equal to what's in the queue
                                 thread.event = resume(thread.coro, thread.queue[1])
@@ -143,46 +122,47 @@ local runInternal = function(listener, groups, dead) -- Function to execute thre
                             table.remove(thread.queue, 1)
                         end
                         thread.event = resume(thread.coro, e)
-                    elseif not thread.enabled then -- OTHERWISE if the thread isn't enabled add the event to the thread queue
+                    elseif not thread.enabled then -- OTHERWISE if the thread isn't enabled and isn't dead add the event to the thread queue
                         thread.queue[#thread.queue+1] = e
                     end
                     if coroutine.status(thread.coro) == "dead" then
                         for k = 1, #groups[i].threads do -- Search for the thread to remove
-                            if groups[i].threads[k] == thread then -- Find the thread in the groups table
-                                for l = 1, #static do -- For each thread that was made BEFORE the manager was executed
-                                    if static[l] == thread then -- If this dead one is a static thread
-                                        static_dead = static_dead+1 -- Add it to the dead list
+                            if groups[i].threads[k] == thread then
+                                for l = 1, #origin do
+                                    if groups[i].threads[k] == origin[l] then
+                                        totalDead = totalDead + 1
                                     end
                                 end
-                                table.remove(groups[i].threads, k) -- Remove the thread from execution
+                                table.remove(groups[i].threads, k)
                                 break
                             end
                         end
                     end
                 end
-            else -- OTHERWISE
-                for j = 1, #s_groups[i].threads do -- For each thread in this group
+            else
+                for j = 1, #s_groups[i].threads do
                     local thread = s_groups[i].threads[j]
-                    thread.queue[#thread.queue+1] = e -- Add the event to its own queue
+                    thread.queue[#thread.queue+1] = e
                 end
             end
         end
-        if static_dead >= dead and dead > 0 then
-            return
+        if totalDead >= dead then -- If dead isn't 0 and the current dead is larger or equal to the target amount
+            break -- Get out of the main loop
         end
-        e = {listener()} -- package whatever listener returns
+        e = {os.pullEventRaw()} -- Pull a raw event, package it immediately
     end
 end
 
-this.manager.runGroup = function(listener, group, dead) -- Function to execute thread management for a single group
+this.manager.runGroup = function(group, dead) -- Function to execute thread management for a single group
     group = isgroup(group)
     assert(group, "Invalid argument #1 (valid group expected)")
-    res = runInternal(listener, {group}, dead)
+    local res = runInternal({group}, dead)
+    group.enabled = false
     return res
 end
 
-this.manager.run = function(listener, dead) -- Function to execute thread management for all groups
-    return runInternal(listener, groups, dead)
+this.manager.run = function(dead) -- Function to execute thread management for all groups
+    return runInternal(groups, dead)
 end
 
 this.group() -- Add a master group
